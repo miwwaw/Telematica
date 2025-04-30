9dbfd6b6a8b62e3c79c60b7b9c0d7eeddc71695d
miwwaw <149535177+miwwaw@users.noreply.github.com>
2025-04-30 11:44:30 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Bienvenido al curso de Telemática'
+
+@app.route('/render')
+def render():
+    return render_template('hello.html')
+
+if __name__ == "__main__":
+    app.run (host="0.0.0.0", port=8090)
